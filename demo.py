6abf608d0b35d254cc41b17fcbe9abde9c0efa58
Nikhil Ranjan <nikhilranjan@Nikhils-Air.home>
2020-09-05 21:21:21 +0300

    create a demo file

---
+++ demo.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import time
+import numpy as np
+from transformers import *
+import torch
+
+pretrained_weights = 'bert-base-uncased'
+tokenizer = BertTokenizer.from_pretrained(pretrained_weights)
+model = BertModel.from_pretrained(pretrained_weights)
+
+st.title('This is BERT tokenization example')
+
+x = st.text_input('Type any sentence here:')
+output = tokenizer.tokenize(x)
+
+output
